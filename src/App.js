0075fb9c263ed3fec53e179b874fd24c4c521a5b
Jake Byrd <JakeByrd@quickenloans.com>
2019-05-02 21:45:08 -0400

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Coming Soon...</h1>
       </header>
     </div>
   );
